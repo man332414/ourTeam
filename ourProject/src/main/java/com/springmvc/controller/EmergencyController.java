a0f61d758b29b206539f109ca7a3c298a8901693
yunki2021 <yunkinate@naver.com>
2024-12-20 18:03:09 +0900

    12/20 수정

--- ourProject/src/main/java/com/springmvc/controller/EmergencyController.java
+++ ourProject/src/main/java/com/springmvc/controller/EmergencyController.java
@@ -11,15 +11,18 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import org.springframework.web.servlet.DispatcherServlet;
@@ -27,6 +30,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.w3c.dom.Document;
 
 import com.springmvc.DTO.emergencyRoom;
+import com.springmvc.DTO.parentingDiary;
 import com.springmvc.api.HospitalListAddOpenAPI;
 import com.springmvc.service.EmergencyService;
 
@@ -49,17 +53,17 @@ public class EmergencyController {
 		System.out.println("뷰이동"+list);
 		model.addAttribute("emergencylist",list);
 		
-
 		return "emergencys";
 	}
 		
-	
-	@RequestMapping("/emergency")
-	public String welcome(Model model) {
-		model.addAttribute(model);
-		return "emergency";
+	@GetMapping("/{number}")
+	public String viewEmergency2Detail(@PathVariable int number, Model model) {
+		System.out.println("EmergencyController viewEmergency2Detail(): {number} 진입");
+		emergencyRoom room = emergencyService.getemergencyRoomkByNum(number); // 제품 ID로 조회
+	    model.addAttribute("room", room); // 모델에 추가
+	    return "emergency"; // JSP 파일 이름
 	}
-		
+	
 	
 	@GetMapping("/all")
 	public ModelAndView requestAllRooms() {
@@ -167,7 +171,15 @@ public class EmergencyController {
 	
 	//HospitalListAddOpenAPI
 	
-	 
+	//@GetMapping("delete/{id}") ajax로 변경
+	@DeleteMapping("/delete/{number}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable int number) {
+		System.out.println("============================");
+		System.out.println("000.deleteRoom : 진입 "+ number);
+		emergencyService.deleteRoom(number);
+        return ResponseEntity.noContent().build(); // 204 No Content 응답 반환
+    }
+	
 	@ModelAttribute
 	public void addAttributrs(Model model) {
 		model.addAttribute("addTitle","신규병원등록");
